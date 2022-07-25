e889d9a87370319528c8efb14cd07261ef74ecac
Yang <1074572893@qq.com>
2022-07-25 09:29:43 +0800

    first commit

---
+++ src/yang/learn/demo/A.java
@@ -0,0 +1,27 @@
+package yang.learn.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author XYHG
+ * @version 1.0.0
+ * @ClassName A.java
+ * @Description TODO
+ * @createTime 2022年07月20日 16:00:00
+ */
+public class A {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        updateList(list);
+        System.out.println(list);//
+
+
+    }
+    private static void updateList(List list) {
+        list.remove(1);
+    }
+}
